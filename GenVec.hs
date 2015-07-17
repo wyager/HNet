e62b355b02901bf0ec3329cab89c324f8c69b26c
William Yager <will.yager@gmail.com>
2015-07-16 20:13:40 -0700

    new stuff

--- GenVec.hs
+++
@@ -1,6 +0,0 @@
-import Data.Vector.Generic (Vector, (!))
-
-f :: Vector v a => v a -> a
-f = (! 0)
-
-main = undefined
--- Operations.hs
+++ Operations.hs
@@ -2,6 +2,12 @@ module Operations (
 	(•), (••)
 ) where
 
+-- Odd behavior: When (•) is inlined, program is many times (~22x) slower.
+-- There are multiple ways to trigger inlining.
+--   1. Remove (•) from the export list
+--   2. Uncomment the inline definition beneath (••)
+--   3. Use an INLINE pragma on (•)
+
 import Prelude hiding (zipWith, sum, map)
 import Data.Vector.Generic (Vector, zipWith, sum, map, convert)
 
@@ -10,4 +16,4 @@ a • b = sum (zipWith (*) a b)
 
 (••) :: (Vector v1 (v2 a), Vector v1 a, Vector v2 a, Num a) => v2 a -> v1 (v2 a) -> v2 a
 a •• b = convert (map (a •) b)
-	--where a • b = sum (zipWith (*) a b)
+	--where a • b = sum (zipWith (*) a b) -- Uncomment to inline
